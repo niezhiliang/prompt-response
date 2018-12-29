460c9ba7ca24662757469707a550aa1c4e1f86b9
聂志良 <1132263176@qq.com>
2018-12-29 09:29:38 +0800

    m

--- backend/src/main/java/com/niehziliang/canvas/phone/backend/controller/IndexController.java
+++ backend/src/main/java/com/niehziliang/canvas/phone/backend/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.niehziliang.canvas.phone.backend.controller;
 
+import com.niehziliang.canvas.phone.backend.ImgUtils;
 import com.niehziliang.canvas.phone.backend.entity.UploadMo;
 import com.niehziliang.canvas.phone.backend.socket.SocketServer;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +21,17 @@ public class IndexController {
     @RequestMapping(value = "upload")
     public Object upload(@RequestBody UploadMo uploadMo) {
        if (uploadMo.getBase64() != null && uploadMo.getUserid() != null) {
-           SocketServer.sendMessage(uploadMo.getBase64(),uploadMo.getUserid());
-           System.out.println(uploadMo.getUserid()+"--------"+uploadMo.getBase64());
+           //把base64文件逗号前面的截掉  图片旋转不能有文件前缀
+           int index = uploadMo.getBase64().lastIndexOf(",");
+           String imgPfefix = uploadMo.getBase64().substring(0,index+1);
+
+           //得到要旋转的base64串
+           String base64 = uploadMo.getBase64().replace(imgPfefix,"");
+           //得到旋转后的图片base64 并加上前缀返回前端
+           base64 = imgPfefix + ImgUtils.imgRoate(base64);
+           System.out.println(base64);
+           //websocket 发送给前端
+           SocketServer.sendMessage(base64,uploadMo.getUserid());
        }
         Map<String,String> map  = new HashMap<String,String>();
         map.put("status","20");
